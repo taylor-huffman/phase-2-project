5a7f732d7be9b73409ccc8b810b2abbe97ba7088
Taylor Huffman <taylor@crucialartists.com>
2022-04-05 15:53:23 -0500

    Change login spelling

--- src/components/Account.js
+++ src/components/Account.js
@@ -37,7 +37,7 @@ export default function Account() {
                     href='/login'
                     exact>
                     <Button variant="contained">
-                        Log In
+                        Login
                     </Button>
                 </Link>
             </div>}

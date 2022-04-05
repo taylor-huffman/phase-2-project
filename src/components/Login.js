54c2eb0ade6c2c91f426472e1e5cfcee8197f73f
Taylor Huffman <taylor@crucialartists.com>
2022-04-05 11:22:20 -0500

    Remove debugger and begin indention clean up

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,61 +13,50 @@ export default function Login() {
         e.preventDefault()
         localStorage.setItem('currentUserName', e.target.name.value)
         setUser(e.target.name.value)
-        fetch(`http://localhost:4000/users?name=${localStorage.getItem('currentUserName')}`)
+        fetch(`http://localhost:4000/users?name=${e.target.name.value}`)
         .then(r => r.json())
         .then(data => {
-            debugger
             if (data.length === 0) {
                 fetch('http://localhost:4000/users', {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify({
-                    name: e.target.name.value,
-                    following: []
-                })
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        name: e.target.name.value,
+                        following: []
+                    })
                 })
                 .then(r => r.json())
                 .then(data => {
                     console.log(data)
-            })
+                })
             }
         })
         history.push('/account')
     }
 
-  return (
-    <Container maxWidth='lg'>
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handleOnSubmit}>
-            <Box
-                sx={{
-                    '& > :not(style)': { m: 1, width: '40ch', display: 'flex', flexDirection: 'column', margin: '25px auto' },
-                }}
-                noValidate
-                autoComplete="off"
-                >
-                <TextField id="outlined-basic" label="Name" name='name' variant="outlined" />
-                <Box
-                sx={{
-                    '& > :not(style)': { m: 1, width: '100%', margin: '8px 0' },
-                }}
-                >
-                    <Button
-                        variant="contained"
-                        sx={{
-                            '& > :not(style)': { m: 1, margin: 0, width: '100%' },
-                        }}
-                        type="submit"
-                        >
-                        Log In
-                    </Button>
-                </Box>
-            </Box>
-            </form>
-        </div>
-    </Container>
-  )
+    return (
+        <Container maxWidth='lg'>
+            <div>
+                <h1>Login</h1>
+                <form onSubmit={handleOnSubmit}>
+                    <Box
+                        sx={{'& > :not(style)': { m: 1, width: '40ch', display: 'flex', flexDirection: 'column', margin: '25px auto' },}}
+                        noValidate
+                        autoComplete="off">
+                        <TextField id="outlined-basic" label="Name" name='name' variant="outlined" />
+                        <Box sx={{'& > :not(style)': { m: 1, width: '100%', margin: '8px 0' },}}>
+                            <Button
+                                variant="contained"
+                                sx={{'& > :not(style)': { m: 1, margin: 0, width: '100%' },}}
+                                type="submit">
+                                Log In
+                            </Button>
+                        </Box>
+                    </Box>
+                </form>
+            </div>
+        </Container>
+    )
 }
